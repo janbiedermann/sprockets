2d11cf45eaaa05c3a6a7870b73793fc6d17b012f
Jan Biedermann <jan@kursator.de>
2017-10-27 15:16:00 +0200

    fix

--- lib/sprockets/cache.rb
+++ lib/sprockets/cache.rb
@@ -38,11 +38,11 @@ module Sprockets
   class Cache
     # Builtin cache stores.
     autoload :FileStore,   'sprockets/cache/file_store'
-    autoload :FileStore,   'sprockets/cache/k_file_store'
+    autoload :KFileStore,  'sprockets/cache/k_file_store'
     autoload :MemoryStore, 'sprockets/cache/memory_store'
-    autoload :GdbmStore, 'sprockets/cache/gdbm_store'
-    autoload :TokyoStore, 'sprockets/cache/tokyo_store'
-    autoload :IntCache, 'sprockets/cache/int_cache'
+    autoload :GdbmStore,   'sprockets/cache/gdbm_store'
+    autoload :TokyoStore,  'sprockets/cache/tokyo_store'
+    autoload :IntCache,    'sprockets/cache/int_cache'
 
 
     # Internal: Cache key version for this class. Rarely should have to change
